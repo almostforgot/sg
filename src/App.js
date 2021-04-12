dcc2bb1eec80e12774bc2ea731043a3b973b5c26
Yang Fei <yangfei0777@gmail.com>
2021-04-12 20:37:32 +1000

    Initial conversation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,55 @@
 import Chat, { Bubble, useMessages } from '@sssound1/sgui';
 import '@sssound1/sgui/dist/index.css';
+import { useEffect } from 'react';
+import Rasa from './Rasa'
 
 const App = () => {
   const { messages, appendMsg, setTyping } = useMessages([]);
 
-  function handleSend(type, val) {
-    if (type === 'text' && val.trim()) {
+  function handleSend (type, val, initial=false) {
+    if (type === 'text' && val.trim() && !initial) {
       appendMsg({
         type: 'text',
         content: { text: val },
         position: 'right',
       });
-
       setTyping(true);
-
-      setTimeout(() => {
-        appendMsg({
-          type: 'text',
-          content: { text: 'Bala bala' },
-        });
-      }, 1000);
     }
+
+    new Rasa(rasaHost)
+    .sendMessage(val)
+    .then(data => {
+      const validMessageTypes = ["text", "image", "buttons", "attachment", "list"];
+
+      data.filter((message) =>
+        validMessageTypes.some(type => type in message)
+      ).forEach((message) => {
+        let validMessage = false;
+        for (const property in message) {
+          if (validMessageTypes.includes(property)) {
+            validMessage = true;
+            const content = {};
+            content[property] = message[property];
+            appendMsg({
+              type: property,
+              content,
+            });
+          }
+        }
+        if (validMessage === false)
+          throw Error("Could not parse message from Bot or empty message");
+      })
+    })
   }
 
   function renderMessageContent(msg) {
     const { content } = msg;
     return <Bubble content={content.text} />;
   }
+
+  useEffect(() => {
+    handleSend('text', 'start', true)
+  }, [] );
 
   return (
     <Chat
